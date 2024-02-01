270da2617806a1010156735af44f5d69a1d0ea4c
Harvy Jones Pontillas <harvy.pontillas@technomancer.biz>
2024-02-01 16:30:34 +0800

    DAY Initial commit

--- backend/day-02/day-2-input.py
+++ backend/day-02/day-2-input.py
@@ -1,2 +1,13 @@
-age = input("Enter your age: ")
-print(f"You're {age} years old.")
+first_number, second_number, third_number, sum = 0, 0, 0, 0;
+
+numbers = {
+    'first number': first_number,
+    'second number': second_number,
+    'third number': third_number,
+}
+
+for key, value in numbers.items():
+    value = input(f"Enter the {key}: ")
+    sum += int(value);
+
+print(f"Sum: {sum}");
