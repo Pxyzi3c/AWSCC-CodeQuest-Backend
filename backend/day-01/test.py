7f0c6134f0bf909514118634c8658a745efa6e10
Harvy Jones Pontillas <harvy.pontillas@technomancer.biz>
2024-02-01 14:00:30 +0800

    DAY 1 Initial commit

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,13 @@
-name = None
-age = None
-isMale = None
-department = None
+# OPTION 1
+# my_name = "Harvy Jones Pontillas";
+# name_of_department = "DILG";
+# about_me = "I drink rice"
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+# OPTION 2
+my_info = {
+    'name': "Harvy Jones Pontillas",
+    'name_of_department': "DILG",
+    'about_me': "I love drinking rice",
+};
+
+print(f'Hello, I am {my_info["name"]}. I love {my_info["name_of_department"]} and I love being in the Backend even more! One thing about me is that {my_info["about_me"]}.');
