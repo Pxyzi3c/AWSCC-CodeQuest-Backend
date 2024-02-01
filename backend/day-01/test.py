efabadea3335736a74df4a553fd426b483626c1e
Harvy Jones Pontillas <harvy.pontillas@technomancer.biz>
2024-02-01 14:51:16 +0800

    DAY resolve comments

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,16 +1,14 @@
-# OPTION 1
-# my_name = "Harvy Jones Pontillas";
-# name_of_department = "DILG";
-# about_me = "I drink rice"
+my_name = "Harvy Jones Pontillas";
+name_of_department = "DILG";
+about_me = "I drink rice"
 
-# OPTION 2
 my_info = {
-    'name': "Harvy Jones Pontillas",
-    'name_of_department': "DILG",
-    'about_me': "I love drinking rice",
+    'name': my_name,
+    'name_of_department': name_of_department,
+    'about_me': about_me,
 };
 
-print(f'Hello, I am {my_info["name"]}. 
-      I love {my_info["name_of_department"]} 
-      and I love being in the Backend even more! One thing about me is that {my_info["about_me"]}.'
+print(f'Hello, I am {my_info["name"]}. '
+      f'I love {my_info["name_of_department"]} ' 
+      f'and I love being in the Backend even more! One thing about me is that {my_info["about_me"]}.'
 );
