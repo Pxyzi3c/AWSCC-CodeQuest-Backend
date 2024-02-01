5ff0c4e197cac2202d173c698fec91fbff63be34
Harvy Jones Pontillas <harvy.pontillas@technomancer.biz>
2024-02-01 17:56:02 +0800

    DAY 3 Initial commit

---
+++ backend/day-03/day-3-mini-challenge.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print(f"{number} is a Positive number!")
+elif number < 0:
+    print(f"{number} is a Negative number!")
+else:
+    print("That number is a Zero!")
