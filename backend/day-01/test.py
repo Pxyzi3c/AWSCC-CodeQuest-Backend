7cf74653855c1c659e3b79edc9ba30bdc447a2bf
Harvy Jones Pontillas <harvy.pontillas@technomancer.biz>
2024-02-01 14:43:25 +0800

    DAY 1: Multiline kineme

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -10,4 +10,7 @@ my_info = {
     'about_me': "I love drinking rice",
 };
 
-print(f'Hello, I am {my_info["name"]}. I love {my_info["name_of_department"]} and I love being in the Backend even more! One thing about me is that {my_info["about_me"]}.');
+print(f'Hello, I am {my_info["name"]}. 
+      I love {my_info["name_of_department"]} 
+      and I love being in the Backend even more! One thing about me is that {my_info["about_me"]}.'
+);
