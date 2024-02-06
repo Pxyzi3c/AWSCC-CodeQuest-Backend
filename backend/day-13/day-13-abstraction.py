d5e8f901f8d6b2ada8c06657b4afad2741355ca6
Harvy Jones Pontillas <harvy.pontillas@technomancer.biz>
2024-02-06 19:36:51 +0800

    DAY13 Abstraction

--- backend/day-13/day-13-abstraction.py
+++ backend/day-13/day-13-abstraction.py
@@ -1,12 +1,22 @@
 class Calculator:
     def sum(self, numlist: list):
         _sum = 0
+
         for num in numlist:
             _sum += num
-
+        
         return _sum
 
-numlist = [5, 10, 15, 20, 25]
-new_calculator = Calculator()
-sum = new_calculator.sum(numlist)
-print(sum)
+def start():
+    numbers = []
+    while True:
+        user_input = input("Enter a number [press Enter to exit]: ")
+        if user_input:
+            numbers.append(int(user_input))
+        else:
+            calculate_sum = Calculator().sum(numbers)
+            print(f"Here's the numbers inputted: {numbers}")
+            print(f"Sum: {calculate_sum}")
+            break
+
+start()
