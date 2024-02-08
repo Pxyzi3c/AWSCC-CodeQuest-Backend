fb9ed24e2f4a2ac116a0dea429007036deb8ed2f
Harvy Jones Pontillas <harvy.pontillas@technomancer.biz>
2024-02-08 12:09:00 +0800

    DAY14 Encapsulation

--- backend/day-14/day-14-encapsulation.py
+++ backend/day-14/day-14-encapsulation.py
@@ -1,16 +1,36 @@
 from datetime import datetime
+filePath = 'backend/day-14/student_records.txt'
 
 class Student:
-    def __init__(self, name, birth_yr, birth_mo, birth_day):
+    def __init__(self, name: str, birth_yr: int, birth_mo: int, birth_day: int):
         self.name = name
         self._birthdate = self._to_unixtime(birth_yr, birth_mo, birth_day)
-
-    def _to_unixtime(self, yr, mo, d):
-        return datetime(yr, mo, d).timestamp()        
-
+    
+    def _to_unixtime(self, yr, mo, day):
+        return datetime(yr, mo, day).timestamp()
+    
     @property
     def birthdate(self):
         return datetime.fromtimestamp(self._birthdate).strftime("%B %d, %Y")
 
-student1 = Student("John", 2002, 10, 5)
-print(student1.birthdate)
+def submitStudRecord(studDetails):
+    with open(filePath, 'a') as file:
+        for key, value in studDetails.items():
+            file.write(f"{key}: {value}\n")
+        file.write("\n")
+
+def start():
+    while True:
+        name = input("Enter student name: ")
+        year = int(input("Enter birth year [YYYY]: "))
+        month = int(input("Enter birth month [MM]: "))
+        day = int(input("Enter birth day [DD]: "))
+
+        student = Student(name, year, month, day)
+        submitStudRecord({"Name": student.name, "Birthdate": student.birthdate})
+
+        breaker = input("Continue [y/n] ?").lower()
+
+        if breaker == 'n':
+            break
+start()
